b9ee2222d8b5332b011bcef2fafa8caa4a98d55c
Jan Šindler <jansindl3r@gmail.com>
2024-05-14 11:22:14 +0200

    netlify-toml

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.md$/,
