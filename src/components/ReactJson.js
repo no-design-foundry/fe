88ae2414dc5ba73719299ffd38ec90a3b0b9c56f
Jan Sindler <jansindl3r@gmail.com>
2025-01-28 21:14:12 +0100

    progress

--- src/components/ReactJson.js
+++ src/components/ReactJson.js
@@ -2,69 +2,122 @@ import React, { useState } from "react";
 import { convertXML } from "simple-xml-to-json";
 import { useFela } from "react-fela";
 
-const INDENT = 2
+const INDENT = 1;
 
 const childStyle = ({ level }) => ({
-  // marginLeft: `${level * INDENT}ch`,
-  ...(level === 1 && {
-    padding: 10,
-    background: "#F2F2F2",
-    borderRadius: 10,
-    margin: 10,
-  }),
+  maxWidth: "80vw",
+  extend: [
+    {
+      condition: level > 1,
+      style: {
+        width: "100%",
+      }
+    },
+  ]
 });
 
-const headerRule = ({ level }) => ({
-  textDecoration: "underline",
-  "& > * + *": { marginLeft: 20 },
-  "& + ul": {
-    marginLeft: `${level * INDENT}ch`,
+const buttonRule = ({level}) => ({
+  extend: [{
+    condition: level === 1,
+    style: {
+      padding: 10,
+      background: "#F2F2F2",
+      borderRadius: 10,
+      marginVertical: 2,
+    }
+  }]
+})
+
+
+const headerStyle = ({ level }) => ({
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  // marginLeft: `${level * INDENT}ch`,
+});
+
+const lastChildStyle = ({ level }) => ({
+  display: "flex",
+  "& > * + *": {
+    marginLeft: "10px",
+  },
+  extend: [
+    {
+      condition: level > 0,
+      style: {
+        "&:not(:first-child)": {
+          borderTop: "1px solid black",
+        },
+        paddingVertical: "4px",
+      }
+    }
+  ]
+})
+
+const getIndentRule = (level) => () => ({
+  extend: [{
+    condition: level > 1,
+    style: {
+      marginLeft: `${level * INDENT}ch`,
+    }
+  }]
+});
+
+const indicatorRule = {
+  textDecoration: "none",
+  marginLeft: "auto",
+}
+
+const attributesRule = () => ({
+  whiteSpace: "pre-wrap",
+  marginLeft: "10px",
+  "& > * + *": {
+    marginLeft: "10px",
   }
 })
 
-const lastChildStyle = () => ({
-  display: "flex",
-  "& > * + *": { marginLeft: 20 },
-});
-
-function LastChildElement({ data }) {
-  return Object.entries(data).map(([key, value]) => (
-    <span key={key}>
-      {key}={value}
+function Attributes({ data }) {
+  const {css} = useFela()
+  return (
+    <span className={css(attributesRule)}>
+      {Object.entries(data).map(([key, value]) => (
+        <span key={key}>
+          {`${key}=${value}`}
+        </span>
+      ))}
     </span>
-  ));
+  );
 }
 
 function ChildElement({ data, level = 0 }) {
   const { css } = useFela({ level });
   const key = Object.keys(data)[0];
-  const { children, ...otherValues } = data[key];
-  const [expanded, setExpanded] = useState(!false);
+  const { children, ...attributes } = data[key];
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className={css(childStyle)}>
-      <div
-        className={css(headerRule)}
-        {...(children && { onClick: () => setExpanded(!expanded) })}
-      >
+    <div className={css(buttonRule, childStyle)}>
+      <div className={css(headerStyle)} onClick={() => children && setExpanded(!expanded)}>
         <span>{key}</span>
-        <LastChildElement data={otherValues} />
+        {attributes && <Attributes data={attributes} />}
+        {children && <span className={css(indicatorRule)}>{expanded ? "▲" : "▼"}</span>}
       </div>
       {expanded && (
-        <ul>
-          {children?.map((item, index) => {
-            const [key, value] = Object.entries(item)[0];
+        <ul className={css(getIndentRule(level + 1))} >
+          {children?.map((child, index) => {
+            const childKey = Object.keys(child)[0];
+            const childData = child[childKey];
             return (
-              <div className={css(lastChildStyle)} key={index}>
-                {value.children ? (
-                  <ChildElement key={index} data={item} level={level + 1} />
+              <li key={index} className={css(lastChildStyle)}>
+                {childData.children ? (
+                  <ChildElement data={child} level={level + 1} />
                 ) : (
-                  <>
-                    <span>{key}</span>
-                    <LastChildElement data={item[key]} />
-                  </>
+                  <div className={css(buttonRule({level: level + 1}))}>
+                    <span>{childKey}</span>
+                    <Attributes data={childData} />
+                  </div>
                 )}
-              </div>
+              </li>
             );
           })}
         </ul>
@@ -75,11 +128,7 @@ function ChildElement({ data, level = 0 }) {
 
 function ReactJson({ src }) {
   const json = convertXML(src);
-  return (
-    <div>
-      <ChildElement data={json} />
-    </div>
-  );
+  return <ChildElement data={json} />;
 }
 
 export default ReactJson;
